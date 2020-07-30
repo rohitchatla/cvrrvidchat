c49236dfb03ba14c0dc19b10379b3e6ef183cc94
CV Rohit <51963891+rohitchatla@users.noreply.github.com>
2020-07-30 21:45:28 +0530

    hhhhh
    final commit

--- Readme.txt
+++ Readme.txt
@@ -1,2 +1,4 @@
 to start peer server::
 peerjs --port 3001
+
+"start": "concurrently --kill-others \"nodemon server.js\" \"peerjs --port 443\""
--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
+const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+app.get("/", (req, res) => {
+  console.log("starting in server 24321142384724657146527485146527467");
+  res.redirect(`/${uuidV4()}`);
+});
 
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-  	console.log(roomId,userId)
-    socket.join(roomId)
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    console.log(roomId, userId);
+    socket.join(roomId);
 
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.to(roomId).broadcast.emit("user-connected", userId);
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
 
-server.listen(3000)
+server.listen(port);
